c261d8cb43918521cc5a9f09bdc6354f26eb659b
mathew <mathewdel88@gmail.com>
2020-02-19 00:12:38 +0530

    test2

--- app/src/main/java/com/mathew/ocr/SplashScreen.kt
+++ app/src/main/java/com/mathew/ocr/SplashScreen.kt
@@ -7,7 +7,7 @@ import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
-//first
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
