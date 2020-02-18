d3e96e66efdb432e6727954348b7291164c86c21
mathew <mathewdel88@gmail.com>
2020-02-19 01:09:32 +0530

    test23

--- app/src/main/java/com/mathew/ocr/SplashScreen.kt
+++ app/src/main/java/com/mathew/ocr/SplashScreen.kt
@@ -10,7 +10,7 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//ttestttttyy
+//test
         setContentView(R.layout.activity_splash_screen)
         object : CountDownTimer(2000, 1000) {
 
